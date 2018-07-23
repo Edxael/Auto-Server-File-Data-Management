f286eb04c1e8faa0c9310c1b09ca2b8295148020
Edmundo rubio <edmundo@webaholics.co>
2018-07-23 10:15:40 -0600

    update sever.js

--- ExpServFiles/knex.js
+++ ExpServFiles/knex.js
@@ -6,7 +6,7 @@ const knex = require('knex')({
     client: 'pg',
     connection: {
         host: 'localhost',
-        database: 'Singers1',
+        database: 'My1App-Exp',
         user: process.env.PG_User,
         password: process.env.PG_Pass
     }
